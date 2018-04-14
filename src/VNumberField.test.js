1e22d85fc805f097aa821a985340b0d19d7dbabe
Paul Peavyhouse <pv@swooby.com>
2018-04-13 20:51:11 -0700

    Cleanup and comments

--- src/VNumberField.test.js
+++ src/VNumberField.test.js
@@ -5,3 +5,18 @@ test('it works', () => {
   const wrapper = mount(VNumberField)
   expect(wrapper.isVueInstance()).toBe(true)
 })
+
+/*
+  scripts: {
+    "prepublishOnly": "npm test && npm run build",
+  }
+
+  devDependencies: {
+    "jest-css-modules": "^1.1.0",
+    "jest-serializer-html": "^5.0.0",
+    "jest-vue-preprocessor": "^1.4.0",
+  }
+
+https://github.com/egoist/poi/issues/416
+
+*/
